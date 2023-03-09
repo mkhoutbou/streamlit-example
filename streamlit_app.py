a19303ff9cf26aa04597d2dd2ea82ef5901a78ab
Mouhamed Khoutbou Thiam <khoutbou7@gmail.com>
2023-03-09 16:48:35 +0000

    first from chatgpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-# Create a Streamlit app
-st.title("CSV Data Analysis")
+# Load the CSV file
+csv_file = 'Cooperative Assessment For Streamlit.csv'
+df = pd.read_csv(csv_file)
 
-# Load the CSV file into a pandas DataFrame
-df = pd.read_csv('Cooperative Assessment For Streamlit.csv')
+# Display all columns
+st.write('## All Columns')
+st.write(df.columns)
 
-# Display the DataFrame using Streamlit's dataframe function
-st.write(df)
+# Provide basic analysis
+st.write('## Basic Analysis')
+st.write(f'Number of Rows: {df.shape[0]}')
+st.write(f'Number of Columns: {df.shape[1]}')
+st.write(df.describe())
+
+# Allow user to select a column for further analysis
+selected_column = st.selectbox('Select a Column for Further Analysis', df.columns)
+
+# Display unique values in the selected column
+st.write(f'Unique Values in {selected_column}:')
+st.write(df[selected_column].unique())
