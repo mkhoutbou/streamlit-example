8cd996103ceeabdbf9b226b4b4db57fc4dd3f6ec
Mouhamed Khoutbou Thiam <khoutbou7@gmail.com>
2023-03-10 14:52:44 +0000

    second from chatgpt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,41 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import seaborn as sns
 
 # Load the CSV file
 csv_file = 'Cooperative Assessment For Streamlit.csv'
-df = pd.read_csv(csv_file)
 
-# Display all columns
-st.write('## All Columns')
-st.write(df.columns)
 
-# Provide basic analysis
-st.write('## Basic Analysis')
-st.write(f'Number of Rows: {df.shape[0]}')
-st.write(f'Number of Columns: {df.shape[1]}')
-st.write(df.describe())
+# Load data
+@st.cache_data
+def load_data():
+    df = pd.read_csv(csv_file)
+    return df
 
-# Allow user to select a column for further analysis
-selected_column = st.selectbox('Select a Column for Further Analysis', df.columns)
+df = load_data()
 
-# Display unique values in the selected column
-st.write(f'Unique Values in {selected_column}:')
-st.write(df[selected_column].unique())
+# Show data
+if st.checkbox("Show raw data"):
+    st.write(df)
+
+# Show summary statistics
+if st.checkbox("Show summary statistics"):
+    st.write(df.describe())
+
+# Show correlation heatmap
+if st.checkbox("Show correlation heatmap"):
+    st.write(sns.heatmap(df.corr(), annot=True, cmap="coolwarm"))
+
+# Show scatter plot
+x_values = st.selectbox("Select x-axis column for scatter plot", df.columns)
+y_values = st.selectbox("Select y-axis column for scatter plot", df.columns)
+if st.button("Generate scatter plot"):
+    st.write(f"Scatter plot between {x_values} and {y_values}")
+    st.write(px.scatter(df, x=x_values, y=y_values))
+
+# Show bar chart
+column_to_plot = st.selectbox("Select column to plot", df.columns)
+if st.button("Generate bar chart"):
+    st.write(f"Bar chart of {column_to_plot}")
+    st.write(px.bar(df, x=column_to_plot, y=y_values))
